2f684fd49ebab193b5d28b45deb70f6f39068fc8
Gizyz <47669176+Gizyz@users.noreply.github.com>
2022-03-28 20:54:40 +0200

    saver

--- js/script.js
+++ js/script.js
@@ -1,95 +1,114 @@
 var cookieEl = document.getElementById("clickCookie");
-var upgradeTabEl = document.getElementById("workerTab")
+var upgradeTabEl = document.getElementById("workerTab");
 var cookiesEl = document.getElementById("cookies");
 var cpsEl = document.getElementById("cps");
 
-
+//Clicks per second
 var oldClick = 0;
+var cps = 0;
 //Cookies
 var cookieAmount = 0;
 
 //Workers
 var workerClicksPerSecond = 0;
+var workerEl = [];
+var workerCostEl = [];
+var workerAmountEl = [];
 
 const workers = [
-    {name: "Clicker", cost: 10, amount: 0, clicks: 0.2},
-    {name: "Click mamas", cost: 100, amount: 0, clicks: 5},
-    {name: "Click mamamas", cost: 500, amount: 0, clicks: 50},
-    {name: "Super Clickers", cost: 1500, amount: 0, clicks: 200},
-    {name: "Super Clickers", cost: 1500, amount: 0, clicks: 200},
-    {name: "Super Clickers", cost: 1500, amount: 0, clicks: 200}
-]
+  { name: "Clicker", cost: 10, amount: 0, clicks: 0.2 },
+  { name: "Click mamas", cost: 100, amount: 0, clicks: 5 },
+  { name: "Click mamamas", cost: 500, amount: 0, clicks: 50 },
+  { name: "Super Clickers", cost: 1500, amount: 0, clicks: 200 },
+  { name: "Super Clickers", cost: 1500, amount: 0, clicks: 200 },
+  { name: "Super Clickers", cost: 1500, amount: 0, clicks: 200 },
+];
 
+cookieEl.addEventListener("click", clicker);
 
-//Clicker
-var workerClickerEl = document.querySelector("#worker_clicker");
-var clickerAmountEl = document.querySelector("#clickerAmount");
-var clickerCostEl = document.getElementById("clickerCost");
+//Initialize
+createWorkers();
 
-
-createWorkers()
+//Creates elements for all worker objects in workers array
 function createWorkers() {
-    for(i=0; i < workers.length; i++){
-        upgradeTabEl.innerHTML += ("<div id='worker"+i+"' class='workerContainer unselectable'>" +
-                "<div>" +
-                    "<h1>"+workers[i].name+"</h1>" + 
-                    "<p>"+workers[i].cost+" cookies</p>" + 
-                "</div>" + 
-                "<p>"+workers[i].amount+"</p>" +
-            "</div>")
-        //workerEl = document.addEventListener('worker')
-        //worker.addEventListener('click')
-    }
+  for (i = 0; i < workers.length; i++) {
+    upgradeTabEl.innerHTML +=
+      "<div id='worker" +
+      i +
+      "'" +
+      "class='workerContainer' onclick='workerClickEvent(" +
+      i +
+      ")'>" +
+      "<div>" +
+      "<h1>" +
+      workers[i].name +
+      "</h1>" +
+      "<p id='worker" +
+      i +
+      "Cost'>" +
+      workers[i].cost +
+      " cookies</p>" +
+      "</div>" +
+      "<p id='worker" +
+      i +
+      "Amount'>" +
+      workers[i].amount +
+      "</p>" +
+      "</div>";
+
+    workerEl[i] = document.getElementById("worker" + i);
+    workerCostEl[i] = document.getElementById("worker" + i + "Cost");
+    workerAmountEl[i] = document.getElementById("worker" + i + "Amount");
+  }
+}
+function workerClickEvent(i) {
+  console.log("Button: " + i);
+  if (cookieAmount >= workers[i].cost) {
+    cookieAmount -= workers[i].cost;
+    workers[i].amount++;
+
+    workers[i].cost += Math.floor(0.5 * workers[i].amount);
+  }
+  cookieDisplay();
 }
 
-
-
-cookieEl.addEventListener('click', clicker);
-
-workerClickerEl.addEventListener('click', workerClicker);
-
 //cps reset
-setInterval(function (){
-    cps = 0;
-    
-    workerClicks();
-    cookieDisplay();
+setInterval(function () {
+  cps = 0;
+  workerClicks();
+  cookieDisplay();
 }, 1000);
 
-
 function workerClicks() {
-    workerClicksPerSecond = 0
-    for (i=0;i<workers.length;i++){
-        workerClicksPerSecond += (workers[i].amount * workers[i].clicks)
-    }
-    cookieAmount += workerClicksPerSecond
+  workerClicksPerSecond = 0;
+  for (i = 0; i < workers.length; i++) {
+    workerClicksPerSecond += workers[i].amount * workers[i].clicks;
+  }
+  cookieAmount += workerClicksPerSecond;
 }
 
 function clicker() {
-    cookieAmount++
-    cookieDisplay()
-    clicksPerSecond();
+  cookieAmount++;
+  cookieDisplay();
+  clicksPerSecond();
 }
 function clicksPerSecond() {
-    var newClick = Math.floor(Date.now())
-    var cps = (1000/(newClick - oldClick)).toFixed(2);
-    oldClick = newClick;
-  
-    cpsEl.innerHTML = cps + " cps"
+  var newClick = Math.floor(Date.now());
+  cps = (1000 / (newClick - oldClick)).toFixed(2);
+  oldClick = newClick;
+
+  cpsEl.innerHTML = cps + " cps";
 }
 
-function workerClicker() {
-    if (cookieAmount >= workers[0].cost) {
-        cookieAmount -= workers[0].cost;
-        workers[0].amount++
-
-        workers[0].cost += Math.floor(0.5 * workers[0].amount)
-    }
-    cookieDisplay()
-}
 function cookieDisplay() {
-    cookiesEl.innerHTML = cookieAmount.toFixed(1) + " cookies";
-    cpsEl.innerHTML = cps + " cps";
-    clickerAmountEl.innerHTML = workers[0].amount;
-    clickerCostEl.innerHTML = workers[0].cost + " cookies";
+  cookiesEl.innerHTML = cookieAmount.toFixed(1) + " cookies";
+  cpsEl.innerHTML = cps + " cps";
+
+  for (i = 0; i < workers.length; i++) {
+    workerAmountEl[i].innerHTML = workers[i].amount;
+    console.log(workerAmountEl[i]);
+    workerCostEl[i].innerHTML = workers[i].cost + " cookies";
+    console.log("cost: " + workers[i].cost);
+    console.log(i);
+  }
 }
